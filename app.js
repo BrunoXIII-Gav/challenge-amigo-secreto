67f2113383ebe677be165a48f2236f26c3ae6837
BrunoXIII-Gav <bruno.gavidia@upch.pe>
2025-03-07 19:55:43 -0500

    segundo cambio

--- app.js
+++ app.js
@@ -1,4 +1,68 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 
-//hey
+let listaAmigoSecreto = [];
+
+
+
+function agregarAmigo(){
+
+    let texto = document.getElementById("amigo").value;
+    
+    if (texto === "" || !isNaN(texto)){
+        alert("Por favor, ingresa un nombre valido")
+        return;
+    }
+
+
+    let amigoNuevo = document.getElementById("amigo").value;
+    
+    listaAmigoSecreto.push(amigoNuevo);
+
+    console.log(listaAmigoSecreto)
+
+    verListaAmigos();
+
+
+}
+
+function verListaAmigos(){
+    
+    let listaHtml = document.getElementById("listaAmigos");
+    listaHtml.innerHTML = "";
+    
+    for(let i = 0; i<listaAmigoSecreto.length;i++){
+
+        let verAmigo = document.createElement("li");
+        verAmigo.textContent = listaAmigoSecreto[i];
+
+        listaHtml.appendChild(verAmigo);
+        
+    }
+    
+
+}
+
+
+function sortearAmigo(){
+
+    let numeroSecreto = generarNumeroSecretoIndice();
+    console.log(numeroSecreto);
+
+    let mensajeGanador = `El amigo secreto sorteado es: ${listaAmigoSecreto[numeroSecreto]}`
+    
+    return document.getElementById("resultado").textContent = mensajeGanador
+
+
+}
+
+
+function generarNumeroSecretoIndice(){
+
+    let tamañoLista = listaAmigoSecreto.length;
+
+    return Math.floor(Math.random()*tamañoLista)
+
+}
+
+
